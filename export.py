1c278afd436642ec3150280a18fce5d258508df2
Clint Daniels <clint.daniels@sandag.org>
2016-04-15 15:27:33 -0700

    All of the neighborhood_vars models are running now. I don't know how to look at results, but they are running. The input data still needs to be cleaned.

--- export.py
+++ export.py
@@ -7,10 +7,11 @@ urbansim_engine = create_engine(get_connection_string("configs/dbconfig.yml", 'u
 
 nodes_sql = 'SELECT node as node_id, x, y FROM urbansim.nodes'
 edges_sql = 'SELECT from_node as [from], to_node as [to], distance as [weight] FROM urbansim.edges'
-parcels_sql = 'SELECT parcel_id, parcel_acres as acres, centroid.STX as x, centroid.STY as y FROM urbansim.parcels'
-buildings_sql = 'SELECT id as building_id, parcel_id, residential_units, year_built FROM urbansim.buildings'
+parcels_sql = 'SELECT parcel_id, luz_id, parcel_acres as acres, centroid.STX as x, centroid.STY as y FROM urbansim.parcels'
+buildings_sql = 'SELECT id as building_id, parcel_id, development_type_id as building_type_id, residential_units, non_residential_sqft, 0 as non_residential_rent_per_sqft, year_built FROM urbansim.buildings'
 households_sql = 'SELECT household_id, building_id, persons, age_of_head, income, children FROM urbansim.households'
 jobs_sql = 'SELECT job_id, building_id, sector_id FROM urbansim.jobs'
+building_sqft_per_job_sql = 'SELECT luz_id, development_type_id, sqft_per_emp FROM urbansim.building_sqft_per_job'
 
 nodes_df = pd.read_sql(nodes_sql, urbansim_engine, index_col='node_id')
 edges_df = pd.read_sql(edges_sql, urbansim_engine)
@@ -18,6 +19,10 @@ parcels_df = pd.read_sql(parcels_sql, urbansim_engine, index_col='parcel_id')
 buildings_df = pd.read_sql(buildings_sql, urbansim_engine, index_col='building_id')
 households_df = pd.read_sql(households_sql, urbansim_engine, index_col='household_id')
 jobs_df = pd.read_sql(jobs_sql, urbansim_engine, index_col='job_id')
+building_sqft_per_job_df = pd.read_sql(building_sqft_per_job_sql, urbansim_engine)
+
+building_sqft_per_job_df.sort_values(['luz_id', 'development_type_id'], inplace=True)
+building_sqft_per_job_df.set_index(['luz_id', 'development_type_id'], inplace=True)
 
 edges_df.sort_values(['from', 'to'], inplace=True)
 #edges_df.set_index(['from', 'to'], inplace=True)
@@ -29,3 +34,4 @@ with pd.HDFStore('data/urbansim.h5', mode='w') as store:
     store.put('buildings', buildings_df, format='t')
     store.put('households', households_df, format='t')
     store.put('jobs', jobs_df, format='t')
+    store.put('building_sqft_per_job', building_sqft_per_job_df, format='t')
--- simulation.py
+++ simulation.py
@@ -2,11 +2,34 @@ import os
 import pandas as pd
 import pandana as pdna
 import urbansim.sim.simulation as sim
+from urbansim.utils import misc
 from urbansim_defaults import models
+
+#sim.broadcast('building_sqft_per_job', 'buildings', cast_index=True, onto_on=['luz_id','building_type_id'])
+
+@sim.table('building_sqft_per_job', cache=True)
+def building_sqft_per_job(store):
+    return store['building_sqft_per_job']
+
+@sim.column('buildings', 'sqft_per_job', cache=True)
+def sqft_per_job(buildings, building_sqft_per_job):
+    bldgs = buildings.to_frame(['luz_id', 'building_type_id'])
+    merge_df = pd.merge(bldgs, building_sqft_per_job.to_frame(), how='left', left_on=['luz_id', 'building_type_id'], right_index=True)
+    merge_df.sqft_per_emp.fillna(-1, inplace=True)
+    merge_df.loc[merge_df.sqft_per_emp < 40, 'sqft_per_emp'] = 40
+    return merge_df.sqft_per_emp
+
+@sim.column('buildings', 'luz_id')
+def luz_id(buildings, parcels):
+    return misc.reindex(parcels.luz_id, buildings.parcel_id)
 
 @sim.column('parcels', 'parcel_acres')
 def parcel_acres(parcels):
     return parcels.acres
+
+@sim.injectable('building_sqft_per_job', cache=True)
+def building_sqft_per_job(settings):
+    return settings['building_sqft_per_job']
 
 @sim.model('build_networks')
 def build_networks(parcels):
@@ -22,10 +45,9 @@ def build_networks(parcels):
     p['node_id'] = net.get_node_ids(p['x'], p['y'])
     sim.add_table("parcels", p)
 
-sim.run(["build_networks"])
-
-sim.run(['neighborhood_vars'])
+sim.run(['build_networks', 'neighborhood_vars'])
 
 nodes = sim.get_table('nodes')
+print nodes.to_frame()
 
 #print nodes.toframe(nodes.local_columns)
